a25596adce88c48cc6cc62aa0ddecb4b5f89d970
artem.ch <artem.ch@rockalab.com>
2022-05-15 01:20:05 +0300

    add swoole web socket server

--- app/index.php
+++ app/index.php
@@ -1,8 +1,38 @@
-<?php
+<!doctype html>
+<html>
+<head>
+    <title>Тест WebSocket</title>
+</head>
 
-$str = '';
-for ($i = 0; $i < 1000000; $i++) {
-    $str .= 's';
-}
+<body>
+<script>
+    let socket = new WebSocket("ws://127.0.0.1:8099");
 
-echo "Hello fpm, {$str}\n";
+    socket.onopen = function(e) {
+        alert("[open] Соединение установлено");
+        alert("Отправляем данные на сервер");
+        socket.send("Меня зовут Джон");
+    };
+
+    socket.onmessage = function(event) {
+        console.log(`[message] Данные получены с сервера: ${event.data}`);
+    };
+
+    socket.onclose = function(event) {
+        if (event.wasClean) {
+            alert(`[close] Соединение закрыто чисто, код=${event.code} причина=${event.reason}`);
+        } else {
+            // например, сервер убил процесс или сеть недоступна
+            // обычно в этом случае event.code 1006
+            alert('[close] Соединение прервано');
+        }
+    };
+
+    socket.onerror = function(error) {
+        alert(`[error] ${error.message}`);
+    };
+
+</script>
+</body>
+
+</html>
